13dbf69bde39741b8d5d55e2c138a4cf9e30d718
oiz5201618 <oiz5201618@gmail.com>
2016-02-26 22:39:00 +0800

    1st_opt

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -2,6 +2,7 @@
 #define _PHONEBOOK_H
 
 #define MAX_LAST_NAME_SIZE 16
+#define OPT 1
 
 /* original version */
 typedef struct __PHONE_BOOK_ENTRY {
@@ -16,6 +17,15 @@ typedef struct __PHONE_BOOK_ENTRY {
     char state[2];
     char zip[5];
     struct __PHONE_BOOK_ENTRY *pNext;
+} entry1;
+
+entry1 *findName1(char lastname[], entry1 *pHead);
+entry1 *append1(char lastName[], entry1 *e);
+
+typedef struct __LASTNAME_ONLY_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    entry1 *detail;
+    struct __LASTNAME_ONLY_ENTRY *pNext;
 } entry;
 
 entry *findName(char lastname[], entry *pHead);
